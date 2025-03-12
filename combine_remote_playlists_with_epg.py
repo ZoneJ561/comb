908a5c65e420d8106dc224aeecfa77bfa6de266c
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-03-12 13:05:01 -0400

    Update combine_remote_playlists_with_epg.py

--- combine_remote_playlists_with_epg.py
+++ combine_remote_playlists_with_epg.py
@@ -6,7 +6,6 @@ playlists = [
     "https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/us_moveonjoy.m3u",
     "https://raw.githubusercontent.com/mikekaprielian/rtnaodhor93n398/refs/heads/main/en/videoall.m3u",
     "https://raw.githubusercontent.com/pigzillaaaaa/iptv-scraper/refs/heads/main/tvpass-all.m3u8",
-    "https://tvpass.org/playlist/m3u",
     "https://raw.githubusercontent.com/BuddyChewChew/iptv-scraper/refs/heads/main/daddylive-channels-tivimate.m3u8",
     "https://raw.githubusercontent.com/BuddyChewChew/iptv-scraper/refs/heads/main/topembed-channels-tivimate.m3u8"
     
