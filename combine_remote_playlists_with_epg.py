098673229282aa83e63c2cb4f8fd0b63265fba52
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-03-12 16:39:32 -0400

    Update combine_remote_playlists_with_epg.py

--- combine_remote_playlists_with_epg.py
+++ combine_remote_playlists_with_epg.py
@@ -3,7 +3,8 @@ import requests
 # Playlist URLs to combine
 playlists = [
     "https://raw.githubusercontent.com/mikekaprielian/rtnaodhor93n398/refs/heads/main/en/videoall.m3u",
-    "https://raw.githubusercontent.com/pigzillaaaaa/iptv-scraper/refs/heads/main/tvpass-all.m3u8"
+    "https://raw.githubusercontent.com/pigzillaaaaa/iptv-scraper/refs/heads/main/tvpass-all.m3u8",
+    "https://raw.githubusercontent.com/BuddyChewChew/iptv-scraper/refs/heads/main/daddylive-channels-tivimate.m3u8"
 ]
 
 # Combined EPG URL
